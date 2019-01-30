f8c6b211c4730deef8c5524b50a4cea581516aa2
James McCann <jamesredux@gmail.com>
2019-01-30 19:31:30 +0700

    used gem high-voltage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#show', page: "home"
-
-  get '/pages/*page' => 'pages#show'
   
+
+
+  
+  HighVoltage.configure do |config|
+  	config.home_page = 'home'
+  end	
 
 end
