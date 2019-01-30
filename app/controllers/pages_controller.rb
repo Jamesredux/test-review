a1f185716af47e4110750be6a58acae599d56137
James McCann <jamesredux@gmail.com>
2019-01-30 18:56:19 +0700

    ---

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
 	def show
 		if valid_page?
-			render template: "pages/#{params[:page]}"
+			render template: "pages#{params[:page]}"
 		else
 			render file: "public/404.html", status: :not_found
 		end		
@@ -12,7 +12,7 @@ class PagesController < ApplicationController
 	private 
 	def valid_page?
 		File.exist?(Pathname.new(Rails.root + 
-																	"app/views/pages/#{params[:page]}.html.erb"))
+																	"app/views/pages#{params[:page]}.html.erb"))
 	
 		
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'pages#show', page: "home"
 
   get '/pages/*page' => 'pages#show'
-
+  
 
 end
